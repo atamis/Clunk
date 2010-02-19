165f075821aa3735966fd9324314a5106c0f3b3c
indigo747 <atamiser@gmail.com>
2010-02-18 17:59:16 -0800

    Updated version number and packages.

---
+++ pkg/clunk-1.0/lib/auto_clunk.rb
@@ -0,0 +1,65 @@
+
+require 'clunk'
+module Clunk
+  
+  # A simple method of using Clunk. Example:
+  #
+  # 
+  #   require 'auto_clunk'
+  #
+  #   Clunk::AutoClunk.goes :TestingAutoClunk
+  #
+  #   class TestingAutoClunk < Clunk::AutoClunk
+  #     def juliet
+  #       puts "Do not deny to him that you love me."
+  #     end
+  #   end
+  #
+  # If you run this, it should allow you to type in "juliet" and get something
+  # back. Isn't it awesome?
+  class AutoClunk < CommandList
+    # Generaly, you don't call this. However, you might override. See
+    # create_cli_from_prompt for information.
+    def initialize
+      @cli = Clunk::CLI.new('>>: ', self)
+    end
+
+    # Use this to make an AutoClunk instance with your own prompt. 
+    # AutoClunk#prompt is an alias. How to use:
+    # 
+    #   require 'auto_clunk'
+    #
+    #   Clunk::AutoClunk.goes :PromptAutoClunkTest
+    #   class PromptAutoClunkTest < Clunk::AutoClunk
+    #     def initialize # Override initialize
+    #       create_cli_from_prompt('This is my awesome prompt: ')
+    #     end
+    #   end
+    def create_cli_from_prompt(p)
+      @cli = Clunk.CLI.new(p, self)
+    end
+
+
+    alias_method :prompt, :create_cli_from_prompt
+
+    # Run the CLI. Name choosen for uniqueness. You won't need to use this. This includes
+    # some redimentry exception handling for ArgumentError so you are safe from imporoper 
+    # argument numbers.
+    def activate_awesomeness
+      begin
+        @cli.go
+      rescue ArgumentError => e
+        puts e
+        retry
+      end
+    end
+
+    # The method used to alert the module to the name of your class. Uses
+    # at_exit to do it's magic. I bet there are better ways of doing this.
+    def self.goes name
+      at_exit {
+        eval("#{name.to_s}.new.activate_awesomeness")
+      }
+    end
+  end
+end
